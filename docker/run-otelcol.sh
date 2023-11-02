467a2a19ad2c503931c35a495140e4903b225787
Fabian Stäber <fabian@fstab.de>
2023-11-02 16:17:14 +0100

    Remove obsolete collector feature flag
    Signed-off-by: Fabian Stäber <fabian@fstab.de>

--- docker/run-otelcol.sh
+++ docker/run-otelcol.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./otelcol-contrib-$OPENTELEMETRY_COLLECTOR_VERSION/otelcol-contrib --config=file:./otelcol-config.yaml --feature-gates=pkg.translator.prometheus.NormalizeName > /dev/null 2>&1
+./otelcol-contrib-$OPENTELEMETRY_COLLECTOR_VERSION/otelcol-contrib \
+	--config=file:./otelcol-config.yaml \
+	> /dev/null 2>&1

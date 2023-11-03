1cb91e6ed8a76989980f77e6190e2316d5b906c5
Fabian Stäber <fabian@fstab.de>
2023-11-03 11:33:18 +0100

    Use stable semantic metrics conventions in example dashboards
    Signed-off-by: Fabian Stäber <fabian@fstab.de>

--- run-example.sh
+++ run-example.sh
@@ -8,5 +8,6 @@ if [[ ! -f ./opentelemetry-javaagent.jar ]] ; then
     curl -sOL https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.30.0/opentelemetry-javaagent.jar
 fi
 export OTEL_LOGS_EXPORTER=otlp
+export OTEL_SEMCONV_STABILITY_OPT_IN=http
 export OTEL_RESOURCE_ATTRIBUTES="service.name=example-app,service.instance.id=localhost:8080"
 java -Dotel.metric.export.interval=500 -Dotel.bsp.schedule.delay=500 -javaagent:opentelemetry-javaagent.jar -jar ./target/example-app.jar

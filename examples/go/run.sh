81590c4878d87a0dcc9831e44c561aa600da4530
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-02-02 18:43:12 +0100

    add go example

--- examples/go/run.sh
+++ examples/go/run.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export OTEL_EXPORTER_OTLP_INSECURE="true"
+export OTEL_EXPORTER_OTLP_INSECURE="true" # needed because of https://github.com/open-telemetry/opentelemetry-go/issues/4834
 export OTEL_METRIC_EXPORT_INTERVAL="5000"  # so we don't have to wait 60s for metrics
 export OTEL_RESOURCE_ATTRIBUTES="service.name=example-app,service.instance.id=localhost:8080"
 go run .

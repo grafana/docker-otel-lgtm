5db6fa7714c98df97cf27c756ad4a2350b21e7e8
Francesco Belacca <francesco.belacca@hotmail.it>
2024-05-19 14:45:03 +0000

    fixing oats errors i had locally

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 cd oats/yaml
-go install github.com/onsi/ginkgo/v2/ginkgo
+go install github.com/onsi/ginkgo/v2/ginkgo@latest
 export TESTCASE_TIMEOUT=5m
 export TESTCASE_BASE_PATH=../../examples
 ginkgo -r

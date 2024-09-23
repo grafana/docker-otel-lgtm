ab143c284e99d112548a8d630d24808f08dff7b9
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-23 13:31:28 +0200

    add logback json example

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 cd oats/yaml
 go install github.com/onsi/ginkgo/v2/ginkgo@latest
 export TESTCASE_TIMEOUT=5m

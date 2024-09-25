19cdbe390411df0ca9959846476f00ffa6825d16
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-25 15:15:01 +0200

    use locally build lgtm image in k8s tests (#112)

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+files=$(find examples -name "lgtm.yaml")
+for file in $files; do
+  sed -i 's#.*image: grafana/otel-lgtm:latest.*#&\n          imagePullPolicy: Never#' "$file"
+done
+
 ./build-lgtm.sh
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 cd oats/yaml

5f86f5987b2b74e4136e1b8e7245ed25518a5b97
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-26 09:28:35 +0200

    enable anonymous login

--- docker/run-grafana.sh
+++ docker/run-grafana.sh
@@ -2,5 +2,8 @@
 
 source ./logging.sh
 
+export GF_AUTH_ANONYMOUS_ENABLED=true
+export GF_AUTH_ANONYMOUS_ORG_ROLE=Admin
+
 cd ./grafana
 run_with_logging "Grafana ${GRAFANA_VERSION}" "${ENABLE_LOGS_GRAFANA:-false}" ./bin/grafana server

fc974ffc16a25b77b28ab31c094a4f36574b6c97
Sean Packham <sean.packham@grafana.com>
2024-12-06 10:11:05 +0000

    Add latency and errors, run traffic script for set time (#172)
    * add checkout controller
    
    * add cart controller
    
    * Add latency and errors, run traffic script for set time

--- examples/java/cart/src/main/java/com/grafana/example/CartController.java
+++ examples/java/cart/src/main/java/com/grafana/example/CartController.java
@@ -1,5 +1,10 @@
 package com.grafana.example;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,24 +13,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-import java.util.Optional;
-
 @RestController
 public class CartController {
 
 	private final RestTemplate checkOutRestTemplate = new RestTemplate();
+    private final Random random = new Random(0);
 
 	@GetMapping("/cart")
 	public ResponseEntity<Object> index(@RequestParam("customer") Optional<String> customerId) throws InterruptedException {
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("X-Customer-ID", customerId.orElse("anonymous"));
+
+        // 10ms base request rate with 0-10ms fluctuation
+        Thread.sleep((long) (10 + Math.abs((random.nextGaussian() + 1.0) * 10)));
+
+        // 1% of requests take much longer
+        if (random.nextInt(100) <= 1) {
+          Thread.sleep((long) (Math.abs((random.nextGaussian() + 1.0) * 100.0)));
+        }
 
 		return ResponseEntity
 			.status(HttpStatus.CREATED)
-			.header("Custom-Header", "CustomValue")
 			.body(Map.of(
-				"id", "1"
+                // random positive id between 1-100,000
+				"id", UUID.randomUUID().toString()
 			));
 	}
 }
--- generate-traffic.sh
+++ generate-traffic.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-watch -n 0.3 'curl -s http://localhost:8081/shop'
+# generate a request every 10ms
+# run for 5s and kill
+watch -n 0.01 'curl -s http://localhost:8081/shop' & sleep 60 ; kill $!

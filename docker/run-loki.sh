a7c6ead5a968a60ac1c95c4be243ddb900ce2927
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-11 17:07:22 +0100

    Put data storage for all services in the /data folder so persistent volumes can hold it. (#180)

--- docker/run-loki.sh
+++ docker/run-loki.sh
@@ -2,4 +2,6 @@
 
 source ./logging.sh
 
+mkdir -p /data/loki
+
 run_with_logging "Loki ${LOKI_VERSION}" "${ENABLE_LOGS_LOKI:-false}" ./loki/loki-linux-${TARGETARCH}  --config.file=./loki-config.yaml

b78053862a6bd4d649f06c13cc1d205d2e426c85
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-18 12:57:27 +0100

    update oats (#190)

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -7,10 +7,9 @@ for file in $files; do
   sed -i 's#.*image: grafana/otel-lgtm:latest.*#&\n          imagePullPolicy: Never#' "$file"
 done
 
-./build-lgtm.sh
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 cd oats/yaml
 go install github.com/onsi/ginkgo/v2/ginkgo@latest
 export TESTCASE_TIMEOUT=5m
 export TESTCASE_BASE_PATH=../../examples
-ginkgo -r
+ginkgo

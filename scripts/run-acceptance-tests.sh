f350989a49e77fa383bc9f9bcbb7553992d19c4c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-19 16:43:26 +0100

    add linters

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -8,8 +8,10 @@ for file in $files; do
 done
 
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+example_dir=$(pwd)/examples
+export TESTCASE_TIMEOUT=5m
+export TESTCASE_BASE_PATH=$example_dir
+
 cd ../oats/yaml
 go install github.com/onsi/ginkgo/v2/ginkgo@latest
-export TESTCASE_TIMEOUT=5m
-export TESTCASE_BASE_PATH=../../examples
 ginkgo

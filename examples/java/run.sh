47e054d4420e64812d4d6bd1d4a01d82aa06ec82
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-19 19:02:09 +0100

    update dependencies in shell files (#197)
    * update dependencies in shell files
    
    * update dependencies in shell files

--- examples/java/run.sh
+++ examples/java/run.sh
@@ -5,10 +5,12 @@ set -euox pipefail
 if [[ ! -f ./target/rolldice.jar ]]; then
 	./mvnw clean package
 fi
-version=2.10.0
-jar=opentelemetry-javaagent-${version}.jar
+
+# renovate: datasource=github-releases depName=opentelemetry-java-instrumentation packageName=open-telemetry/opentelemetry-java-instrumentation
+opentelemetry_javaagent_version=2.10.0
+jar=opentelemetry-javaagent-${opentelemetry_javaagent_version}.jar
 if [[ ! -f ./${jar} ]]; then
-	curl -vL https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v${version}/opentelemetry-javaagent.jar -o ${jar}
+	curl -vL https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v${opentelemetry_javaagent_version}/opentelemetry-javaagent.jar -o ${jar}
 fi
 export OTEL_RESOURCE_ATTRIBUTES="service.name=rolldice,service.instance.id=localhost:8080"
 # uncomment the next line to switch to Prometheus native histograms.

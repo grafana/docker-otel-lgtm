786b29725657c0e941060d46299caa6d9730eaf5
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-11 14:55:41 +0100

    generic remote config

--- docker/run-otelcol.sh
+++ docker/run-otelcol.sh
@@ -2,4 +2,11 @@
 
 source ./logging.sh
 
-run_with_logging "OpenTelemetry Collector ${OPENTELEMETRY_COLLECTOR_VERSION}" "${ENABLE_LOGS_OTELCOL:-false}" ./otelcol-contrib/otelcol-contrib --config=file:./otelcol-config.yaml
+config_file="otelcol-config.yaml"
+
+if [[ ${OTEL_EXPORTER_OTLP_ENDPOINT:-} != "-" ]]; then
+  echo "Also enabling OTLP exporter to ${OTEL_EXPORTER_OTLP_ENDPOINT}"
+  config_file="otelcol-config-local-and-remote.yaml"
+fi
+
+run_with_logging "OpenTelemetry Collector ${OPENTELEMETRY_COLLECTOR_VERSION}" "${ENABLE_LOGS_OTELCOL:-false}" ./otelcol-contrib/otelcol-contrib --config=file:./${config_file}

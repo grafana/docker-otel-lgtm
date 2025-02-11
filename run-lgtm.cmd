bb14b803e4ade870e3e10e9245c95aca1af95851
TeBeCo <tebeco@gmail.com>
2025-02-11 21:52:21 +0400

    fix CMD

--- run-lgtm.cmd
+++ run-lgtm.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~run-lgtm.ps1'"
+powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0\run-lgtm.ps1'"
 exit /b %ERRORLEVEL%

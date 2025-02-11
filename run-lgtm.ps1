191784e7ccafdfc92cdf0f306fa219820c339e51
TeBeCo <tebeco@gmail.com>
2025-02-11 21:46:20 +0400

    change avec PR review comment

--- run-lgtm.ps1
+++ run-lgtm.ps1
@@ -1,8 +1,5 @@
 $release_tag = "latest"
 
-# just a little helper to get rid of the tripple backtics. i tend to keep these on the top after args, before logic
-filter Compress { [regex]::Replace($_, '[\s\r\n]+', ' ') }
-
 $supportedContainerRuntime = 'podman', 'docker'
 $containers = 'grafana', 'prometheus', 'loki'
 $image = "docker.io/grafana/otel-lgtm:$release_tag"
@@ -19,28 +16,25 @@ if ($null -eq $containerCommand) {
     return
 }
 
-# wrapping in " to avoid space-escaping, and add & for execute
-$containerCommand = "& `"$containerCommand`""
-
 $containers | ForEach-Object {
     $null = New-Item -ItemType Directory -Path "$path/container/$_" -Force
 }
 
-Invoke-Expression "$containerCommand pull $image"
+& $containerCommand pull $image
 
-$runCommand = @"
-run
-    --name lgtm
-    -p 3000:3000
-    -p 4317:4317
-    -p 4318:4318
-    --rm
-    -ti
-    -v "$path/container/grafana:/data/grafana"
-    -v "$path/container/prometheus:/data/prometheus"
-    -v "$path/container/loki:/loki"
-    -e GF_PATHS_DATA=/data/grafana
+$runCommand = @(
+    'run'
+    '--name', 'lgtm',
+    '-p', '3000:3000'
+    '-p', '4317:4317'
+    '-p', '4318:4318'
+    '--rm'
+    '-ti',
+    '-v', "${path}/container/grafana:/data/grafana"
+    '-v', "${path}/container/prometheus:/data/prometheus"
+    '-v', "${path}/loki:/loki"
+    '-e', "GF_PATHS_DATA=/data/grafana"
     $image
-"@ | Compress
+)
 
-Invoke-Expression "$containerCommand $runCommand"
+& $containerCommand @runCommand

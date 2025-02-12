49f0dc1e63abf992280ad569c19f62d41221abd2
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-12 12:45:26 +0100

    generic send to otlp

--- docker/run-otelcol.sh
+++ docker/run-otelcol.sh
@@ -4,20 +4,31 @@ source ./logging.sh
 
 config_file="otelcol-config.yaml"
 
-#if [[ [[ ! -v variable ]] ${GRAFANA_CLOUD_ZONE:-} != "-" ]]; then
-if [[ -v GRAFANA_CLOUD_ZONE ]]; then
-  if [[ ! -v GRAFANA_CLOUD_INSTANCE_ID ]]; then
-    echo "Please set GRAFANA_CLOUD_INSTANCE_ID to the instance ID of your Grafana Cloud instance"
-    exit 1
-  fi
-  if [[ ! -v GRAFANA_CLOUD_API_KEY ]]; then
-    echo "Please set GRAFANA_CLOUD_API_KEY to the zone of your Grafana Cloud instance"
-    exit 1
-  fi
+if [[ -v OTEL_EXPORTER_OTLP_ENDPOINT ]]; then
+  echo "Also enabling OTLP/HTTP export to ${OTEL_EXPORTER_OTLP_ENDPOINT}"
+  config_file="otelcol-config-export-http.yaml"
 
-  echo "Also enabling Grafana Cloud export to ${GRAFANA_CLOUD_ZONE} with instance ID ${GRAFANA_CLOUD_INSTANCE_ID}"
+  if [[ -v OTEL_EXPORTER_OTLP_HEADERS ]]; then
+    echo "Adding basic auth to OTLP/HTTP export"
+    config_file="otelcol-config-export-http-auth.yaml"
 
-  config_file="otelcol-config-grafana-cloud.yaml"
+    # fail if the headers do not start with "Authorization=Basic "
+    if [[ ! ${OTEL_EXPORTER_OTLP_HEADERS} =~ ^Authorization=Basic\  ]]; then
+      echo "OTEL_EXPORTER_OTLP_HEADERS must start with 'Authorization Basic '"
+      exit 1
+    fi
+    # fail if there are multiple headers, separated by commas
+    if [[ ${OTEL_EXPORTER_OTLP_HEADERS} =~ , ]]; then
+      echo "OTEL_EXPORTER_OTLP_HEADERS must not contain commas"
+      exit 1
+    fi
+
+    # remove "Authorization=Basic " prefix
+    username=$(echo "${OTEL_EXPORTER_OTLP_HEADERS#* }" | base64 -d | cut -d: -f1)
+    password=$(echo "${OTEL_EXPORTER_OTLP_HEADERS#* }" | base64 -d | cut -d: -f2)
+    export OTEL_EXPORTER_OTLP_USERNAME=${username}
+    export OTEL_EXPORTER_OTLP_PASSWORD=${password}
+  fi
 fi
 
 run_with_logging "OpenTelemetry Collector ${OPENTELEMETRY_COLLECTOR_VERSION}" "${ENABLE_LOGS_OTELCOL:-false}" ./otelcol-contrib/otelcol-contrib --config=file:./${config_file}

117b1654af7c93841e1463a43f2615a7c9dfbcfa
Ghassen Rjab <rjab.ghassen@gmail.com>
2025-02-20 13:50:04 +0100

    Add Node.js example (#284)
    With this PR I add example app for Node.js.
    
    The example is based on the Node.js app written in OpenTelemetry
    documentation (dice server)
    
    Even though the OpenTelemetry lib for Logging in Node.js is still in
    development I use it in the example. It is working well
    
    ---------
    
    Co-authored-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

---
+++ examples/nodejs/instrumentation.js
@@ -0,0 +1,32 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node')
+const {
+  getNodeAutoInstrumentations
+} = require('@opentelemetry/auto-instrumentations-node')
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics')
+const {
+  OTLPTraceExporter
+} = require('@opentelemetry/exporter-trace-otlp-proto')
+const {
+  OTLPMetricExporter
+} = require('@opentelemetry/exporter-metrics-otlp-proto')
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter()
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-http': {
+        ignoreIncomingRequestHook: (request) => {
+          if (request.url === '/favicon.ico') {
+            return true
+          }
+          return false
+        }
+      }
+    })
+  ]
+})
+
+sdk.start()

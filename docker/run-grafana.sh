e06a730a856c76d8f0e5af13c5f81d14745c8125
Vincent Carlos <git@finzzz.net>
2025-03-03 16:11:51 +0900

    disable anonymous view

--- docker/run-grafana.sh
+++ docker/run-grafana.sh
@@ -2,9 +2,6 @@
 
 source ./logging.sh
 
-export GF_AUTH_ANONYMOUS_ENABLED=true
-export GF_AUTH_ANONYMOUS_ORG_ROLE=Admin
-
 export GF_PATHS_HOME=/data/grafana
 export GF_PATHS_DATA=/data/grafana/data
 export GF_PATHS_PLUGINS=/data/grafana/plugins

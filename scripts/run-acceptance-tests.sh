51d1633de5a28b29458df29c68f1b8f2b5e74900
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-01 15:37:20 +0200

    fix acceptance test (#367)
    Depends on https://github.com/grafana/oats/pull/97

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -2,16 +2,6 @@
 
 set -euo pipefail
 
-files=$(find examples -name "lgtm.yaml")
-for file in $files; do
-	sed -i 's#.*image: grafana/otel-lgtm:latest.*#&\n          imagePullPolicy: Never#' "$file"
-done
-
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-example_dir=$(pwd)/examples
-export TESTCASE_TIMEOUT=5m
-export TESTCASE_BASE_PATH=$example_dir
-
+cp mise.* ../oats
 cd ../oats/yaml
-go install github.com/onsi/ginkgo/v2/ginkgo@latest
 ginkgo

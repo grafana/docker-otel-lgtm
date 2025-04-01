57125f872f47c2a51971cc00cff3ca042e969f26
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-01 12:47:32 +0200

    fix acceptance test

--- scripts/run-acceptance-tests.sh
+++ scripts/run-acceptance-tests.sh
@@ -2,18 +2,6 @@
 
 set -euo pipefail
 
-RELEASE=${1:-latest}
-
-files=$(find examples -name "lgtm.yaml")
-for file in $files; do
-	sed -i 's#.*image: grafana/otel-lgtm:latest.*#&\n          imagePullPolicy: Never#' "$file"
-done
-
-example_dir=$(pwd)/examples
-export TESTCASE_TIMEOUT=5m
-export TESTCASE_BASE_PATH=$example_dir
-export LGTM_VERSION=$RELEASE
-
 cp mise.* ../oats
 cd ../oats/yaml
 ginkgo

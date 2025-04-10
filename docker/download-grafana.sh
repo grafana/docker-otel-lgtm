39fbfcdf03050f4da964a3bac290e3ec907b43ab
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-10 16:44:48 +0200

    fix docker caching by passing the version

--- docker/download-grafana.sh
+++ docker/download-grafana.sh
@@ -13,5 +13,4 @@ curl -sOL https://dl.grafana.com/oss/release/"${ARCHIVE}"
 CHECKSUM_URL=https://grafana.com/api/downloads/grafana/versions/"${VERSION:1}"/packages/"${TARGETARCH}"/linux
 echo "$(curl -sL "${CHECKSUM_URL}" -H 'accept: application/json' | jq -r '.sha256') ${ARCHIVE}" | sha256sum -c
 tar xfz "${ARCHIVE}"
-rm "${ARCHIVE}"
-mv grafana-"${VERSION}" grafana/
+mv grafana-"${VERSION}" /otel-lgtm/grafana/
--- docker/run-grafana.sh
+++ docker/run-grafana.sh
@@ -11,5 +11,5 @@ export GF_PATHS_HOME=/data/grafana
 export GF_PATHS_DATA=/data/grafana/data
 export GF_PATHS_PLUGINS=/data/grafana/plugins
 
-cd ./grafana
+cd /otel-lgtm/grafana || exit 1
 run_with_logging "Grafana ${GRAFANA_VERSION}" "${ENABLE_LOGS_GRAFANA:-false}" ./bin/grafana server

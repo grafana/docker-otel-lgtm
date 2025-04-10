d0904019610f2c244a5bb79029b0dfd2a7ea3898
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-10 12:50:19 +0200

    fast pyroscope

--- docker/run-pyroscope.sh
+++ docker/run-pyroscope.sh
@@ -4,4 +4,4 @@ source ./logging.sh
 
 mkdir -p /data/pyroscope
 
-run_with_logging "Pyroscope ${PYROSCOPE_VERSION}" "${ENABLE_LOGS_PYROSCOPE:-false}" ./pyroscope/pyroscope --config.file=.todo
+run_with_logging "Pyroscope ${PYROSCOPE_VERSION}" "${ENABLE_LOGS_PYROSCOPE:-false}" ./pyroscope/pyroscope --config.file=./pyroscope-config.yaml

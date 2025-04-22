9430c7c5f6ef81ea4f135198a381d6cf2310d245
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-22 15:58:04 +0200

    add llm plugin

--- docker/run-grafana.sh
+++ docker/run-grafana.sh
@@ -12,11 +12,8 @@ export GF_PATHS_DATA=/data/grafana/data
 export GF_PATHS_PLUGINS=/data/grafana/plugins
 
 # pyroscope settings:
-# prevents a Grafana startup error in case a newer plugin version (set in package.json) is used
-# compared to the latest one published in the catalog
-export GF_PLUGINS_PREINSTALL_DISABLED=true
 # profiles drilldown connects to this plugin automatically - so we install it (even though it does nothing)
-export GF_INSTALL_PLUGINS=grafana-llm-app
+export GF_PLUGINS_PREINSTALL=grafana-llm-app
 
 cd /otel-lgtm/grafana || exit 1
 run_with_logging "Grafana ${GRAFANA_VERSION}" "${ENABLE_LOGS_GRAFANA:-false}" ./bin/grafana server

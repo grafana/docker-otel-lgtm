6b84cb8bf838feadf2745616cf36b863c2b66642
Tim Shilov <tim@shilov.dev>
2025-07-22 11:10:06 +0100

    refactor: formatting inconsistency

--- docker/run-grafana.sh
+++ docker/run-grafana.sh
@@ -14,9 +14,9 @@ export GF_PATHS_PLUGINS=/data/grafana/plugins
 # pyroscope settings:
 # profiles drilldown connects to this plugin automatically - so we install it (even though it does nothing)
 if [ -n "${GF_PLUGINS_PREINSTALL:-}" ]; then
-    export GF_PLUGINS_PREINSTALL="${GF_PLUGINS_PREINSTALL},grafana-llm-app"
+	export GF_PLUGINS_PREINSTALL="${GF_PLUGINS_PREINSTALL},grafana-llm-app"
 else
-    export GF_PLUGINS_PREINSTALL=grafana-llm-app
+	export GF_PLUGINS_PREINSTALL=grafana-llm-app
 fi
 
 cd /otel-lgtm/grafana || exit 1

fabc16cfe272fdcdc304b3c0859ceae56ed3cb78
Martin Costello <martin@martincostello.com>
2025-08-15 10:32:23 +0100

    Fix installation script
    - Trim `v` before extracting.
    - Refactor to reduce number of quote paits.
    
    
    Signed-off-by: Martin Costello <martin@martincostello.com>

--- docker/download-grafana.sh
+++ docker/download-grafana.sh
@@ -8,9 +8,9 @@ if [[ -z "${VERSION}" ]]; then
 	exit 1
 fi
 
-ARCHIVE=grafana-"${VERSION:1}".linux-"${TARGETARCH}".tar.gz
-curl -sOL https://dl.grafana.com/oss/release/"${ARCHIVE}"
-CHECKSUM_URL=https://grafana.com/api/downloads/grafana/versions/"${VERSION:1}"/packages/"${TARGETARCH}"/linux
-echo "$(curl -sL "${CHECKSUM_URL}" -H 'accept: application/json' | jq -r '.sha256') ${ARCHIVE}" | sha256sum -c
+ARCHIVE="grafana-${VERSION:1}".linux-${TARGETARCH}".tar.gz"
+curl -sOL "https://dl.grafana.com/oss/release/${ARCHIVE}"
+CHECKSUM_URL="https://grafana.com/api/downloads/grafana/versions/${VERSION:1}/packages/${TARGETARCH}/linux"
+echo "$(curl -sL "${CHECKSUM_URL}" -H 'accept: application/json' | jq -r '.sha256') "${ARCHIVE}"" | sha256sum -c
 tar xfz "${ARCHIVE}"
-mv grafana-"${VERSION}" /otel-lgtm/grafana/
+mv "grafana-${VERSION:1}" /otel-lgtm/grafana/

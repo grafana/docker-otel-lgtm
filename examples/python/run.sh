8df264f19f60a672468b6703995227f0b4d658c8
Martin Costello <martin@martincostello.com>
2025-08-30 11:59:16 +0100

    Revert env var changes
    Some of them don't seem to benefit from the automatic setting.

--- examples/python/run.sh
+++ examples/python/run.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+export OTEL_METRIC_EXPORT_INTERVAL="5000" # so we don't have to wait 60s for metrics
 export OTEL_RESOURCE_ATTRIBUTES="service.name=rolldice,service.instance.id=127.0.0.1:8082"
 
 python3 -m venv venv

14dd7166b9b4fa941168e6a1cc7407d73caea6c7
Martin Costello <martin@martincostello.com>
2025-08-30 11:37:00 +0100

    Remove redundant env var
    OATs 0.4.1 automatically sets `OTEL_METRIC_EXPORT_INTERVAL=5000`.

--- examples/nodejs/run.sh
+++ examples/nodejs/run.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-export OTEL_METRIC_EXPORT_INTERVAL="5000" # so we don't have to wait 60s for metrics
 export OTEL_SERVICE_NAME="dice-server"
 export OTEL_SERVICE_VERSION="0.1.0"
 

a9ee9cf6b9616d40b42fe32478b159673e2abaaf
Martin Costello <martin@martincostello.com>
2025-10-21 17:30:51 +0100

    Revert debugging change
    Disable logging again.
    
    Signed-off-by: Martin Costello <martin@martincostello.com>

--- docker/run-tempo.sh
+++ docker/run-tempo.sh
@@ -2,4 +2,4 @@
 
 source ./logging.sh
 
-run_with_logging "Tempo ${TEMPO_VERSION}" "true" ./tempo/tempo --config.file=./tempo-config.yaml
+run_with_logging "Tempo ${TEMPO_VERSION}" "${ENABLE_LOGS_TEMPO:-false}" ./tempo/tempo --config.file=./tempo-config.yaml

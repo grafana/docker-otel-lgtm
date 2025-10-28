bf2ea54e34ca6e4f1d14e7d9a880b559ce7b41e1
Willian Braga da Silva <neverping@gmail.com>
2025-10-28 19:11:27 +0100

    chore(scripts): fix script and supports podman
    Most fixes are related to executing the run-lgtm.sh script, but I also changed
    the build scripts and Docker file.
    
    The "container" directory and additional directories required by Grafana, Loki,
    and Prometheus were not present. The "container" directory is now part of the
    ".gitignore" file, meaning that if a newcomer downloads this repository and
    attempts to run the "run-lgtm.sh" script, it won't work. We will create these
    directories if they don't exist.
    
    The second issue comes with the container runtime.
    
    I am a Fedora Workstation user, and because of that, it contains some
    interesting paradigms when running containers.
    
    By default, Fedora uses Podman as its container runtime. I didn't switch to
    Docker for personal reasons.
    
    I added some changes related to the Dockerfile to include the image's full
    address, defaulting to docker.io. I could have used Red Hat's mirror
    suggested by Podman (registry.access.redhat.com). However, Red Hat has a
    rate limit on its own registry. Red Hat also publishes its UBI images to
    Docker Hub. Docker Hub is the most well-known Docker Registry, and some
    people pay for a Docker license, so they don't get rate-limited. It's a
    reasonable choice to use Docker Hub. This prevents Podman from prompting the
    user to select a registry when building the image.
    
    Finally, to address a minor issue with Bind Mounts. I added the "z" flag as
    a mount option, which allows users with SELinux enabled to allow container
    images to manipulate files on the host machine. SELinux comes enabled by
    default on Fedora Workstation. Otherwise, the bind mount works but prevents
    the running container from writing data, resulting in "access denied". The
    "z" flag won´t be an issue on non-Red Hat systems. I tested this flag on
    Debian 11 with KVM.

--- run-lgtm.cmd
+++ run-lgtm.cmd
@@ -1,4 +1,12 @@
 @echo off
 
-powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0\run-lgtm.ps1'"
+set "releasetag=%~1"
+set "local=%~2"
+
+if "%releasetag%"=="" set "releasetag=latest"
+if "%localimg%"=="" set "localimg=0"
+if "%localimg%"=="false" set "localimg=0"
+if "%localimg%"=="true" set "localimg=1"
+
+powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0\run-lgtm.ps1' -ReleaseTag '%releasetag%' -UseLocalImage %localimg%"
 exit /b %ERRORLEVEL%
--- run-lgtm.sh
+++ run-lgtm.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
+set -euo pipefail
+
 RELEASE=${1:-latest}
+LOCAL_VOLUME=${PWD}/container
+# Only set this to "true" if you built the image with the 'build-lgtm.sh' script
+USE_LOCAL_IMAGE=${2:-false}
 
-docker pull docker.io/grafana/otel-lgtm:"${RELEASE}"
+for dir in grafana prometheus loki; do
+	test -d "${LOCAL_VOLUME}"/${dir} || mkdir -p "${LOCAL_VOLUME}"/${dir}
+done
 
-touch .env
+test -f .env || touch .env
 
-docker run \
+if command -v docker >/dev/null 2>&1; then
+	RUNTIME=docker
+	MOUNT_OPTS=rw
+elif command -v podman >/dev/null 2>&1; then
+	RUNTIME=podman
+	# Fedora, by default, runs with SELinux on. We require the "z" option for bind mounts.
+	# See: https://docs.docker.com/engine/storage/bind-mounts/#configure-the-selinux-label
+	# See: https://docs.podman.io/en/stable/markdown/podman-run.1.html section "Labeling Volume Mounts"
+	MOUNT_OPTS="rw,z"
+else
+	echo "Unable to find a suitable container runtime such as Docker or Podman. Exiting."
+	exit 1
+fi
+
+if [ "$USE_LOCAL_IMAGE" = true ]; then
+	if [ "$RUNTIME" = "podman" ]; then
+		# Default address when building with Podman.
+		IMAGE="localhost/grafana/otel-lgtm:latest"
+	else
+		IMAGE="grafana/otel-lgtm:latest"
+	fi
+else
+	IMAGE="docker.io/grafana/otel-lgtm:${RELEASE}"
+	$RUNTIME image pull "$IMAGE"
+fi
+
+$RUNTIME container run \
 	--name lgtm \
 	-p 3000:3000 \
 	-p 4317:4317 \
 	-p 4318:4318 \
 	--rm \
 	-ti \
-	-v "$PWD"/container/grafana:/data/grafana \
-	-v "$PWD"/container/prometheus:/data/prometheus \
-	-v "$PWD"/container/loki:/data/loki \
+	-v "${LOCAL_VOLUME}"/grafana:/data/grafana:"${MOUNT_OPTS}" \
+	-v "${LOCAL_VOLUME}"/prometheus:/data/prometheus:"${MOUNT_OPTS}" \
+	-v "${LOCAL_VOLUME}"/loki:/data/loki:"${MOUNT_OPTS}" \
 	-e GF_PATHS_DATA=/data/grafana \
 	--env-file .env \
-	docker.io/grafana/otel-lgtm:"${RELEASE}"
+	"$IMAGE"

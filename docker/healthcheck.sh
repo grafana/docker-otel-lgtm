478c1a206c8bfb9458e534543356e4695f12ff4d
Cookkiieess <dantesvernon14@gmail.com>
2026-03-04 21:31:37 +0530

    Fix lint issues: remove tabs

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -3,25 +3,25 @@
 set -eu
 
 check_service(){
-	name=$1
-	url=$2
+    name=$1
+    url=$2
 
-	echo "Checking $name"
+    echo "Checking $name"
 
-	set +e
-	#check if port is listening
-	curl -fsS "$url" >/dev/null 2>&1
-	code=$?
-	set -e
+    set +e
+    #check if port is listening
+    curl -fsS "$url" >/dev/null 2>&1
+    code=$?
+    set -e
 
-	if [ "$code" -eq 7 ]; then
-		echo "$name not running (skipping)"
-	elif [ "$code" -eq 0 ]; then
-		echo "$name healthy"
-	else
-		echo "$name unhealthy"
-		exit 1
-	fi
+    if [ "$code" -eq 7 ]; then
+	    echo "$name not running (skipping)"
+    elif [ "$code" -eq 0 ]; then
+	    echo "$name healthy"
+    else
+	    echo "$name unhealthy"
+	    exit 1
+    fi
 }
 
 check_service "Grafana" http://localhost:3000/api/health

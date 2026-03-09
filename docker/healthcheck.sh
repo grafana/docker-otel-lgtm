8a777ed240c3904fbafa6f971340d78737c39564
Cookkiieess <dantesvernon14@gmail.com>
2026-03-09 22:08:44 +0530

    fix: format healthcheck and status issue

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -2,26 +2,26 @@
 
 set -eu
 
-check_service(){
-    name=$1
-    url=$2
+check_service() {
+	name=$1
+	url=$2
 
-    echo "Checking $name"
+	echo "Checking $name"
 
-    set +e
-    #check if port is listening
-    curl -s "$url" >/dev/null 2>&1
-    code=$?
-    set -e
+	set +e
+	# check if port is listening
+	curl -s "$url" >/dev/null 2>&1
+	code=$?
+	set -e
 
-    if [ "$code" -eq 7 ]; then
-        echo "$name not running (skipping)"
-    elif [ "$code" -eq 0 ]; then
-        echo "$name healthy"
-    else
-        echo "$name unhealthy"
-        exit 1
-    fi
+	if [ "$code" -eq 7 ]; then
+		echo "$name not running (skipping)"
+	elif [ "$code" -eq 0 ]; then
+		echo "$name healthy"
+	else
+		echo "$name unhealthy"
+		exit 1
+	fi
 }
 
 check_service "Grafana" http://localhost:3000/api/health
@@ -29,3 +29,6 @@ check_service "Loki" http://localhost:3100/ready
 check_service "Tempo" http://localhost:3200/ready
 check_service "Mimir" http://localhost:9090/-/ready
 check_service "OTel Collector" http://localhost:13133/ready
+
+echo "All running services healthy"
+exit 0

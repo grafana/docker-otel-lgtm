5534d0f07af1ae8d3afc5fa53fefa287735808e2
martincostello <martin@martincostello.com>
2026-04-23 09:49:54 +0100

    Update label
    Replace `_` with `.`.

--- docker/run-pyroscope.sh
+++ docker/run-pyroscope.sh
@@ -9,4 +9,4 @@ if [[ -n "${PYROSCOPE_EXTRA_ARGS:-}" ]]; then
 	read -ra extra_args <<<"${PYROSCOPE_EXTRA_ARGS}"
 fi
 run_with_logging "Pyroscope ${PYROSCOPE_VERSION}" "${ENABLE_LOGS_PYROSCOPE:-false}" \
-	./pyroscope/pyroscope --config.file=./pyroscope-config.yaml -validation.disable-label-sanitization=false "${extra_args[@]}"
+	./pyroscope/pyroscope --config.file=./pyroscope-config.yaml "${extra_args[@]}"
